b3863bd6f50446d69135e836e8a2ca86afac6e38
josmanv <josem_lc@hotmail.com>
2021-05-23 00:59:31 +0200

    interaccion con la api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [busqueda, guradarBusqueda] = useState({
+    ciudad: "",
+    pais: ""
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const {ciudad, pais} = busqueda; 
+
+  useEffect(()=>{
+    const consultarApi = async() => {
+      if(consultar){
+        const miapikey = "bdf8f1193322efd95ab593b7faef3b7d";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${miapikey}`;
+      
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+  
+        console.log(resultado);
+      }
+    }
+    consultarApi();
+  }, [consultar]);
+
   return (
     <Fragment>
     <Header
@@ -14,7 +39,9 @@ function App() {
         <div className="row">
           <div className="col m6 s12">
             <Formulario 
-            
+              busqueda={busqueda}
+              guradarBusqueda={guradarBusqueda}
+              guardarConsultar={guardarConsultar}
             />
           </div>
           <div className="col m6 s12">
